0d9fb3285c7af9b090c1f2e87d7c87992f51fd43
kkami <kkami9090@google.com>
2020-09-29 11:32:02 +0900

    form_app_ex2_09_29

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createRef, useState } from "react";
+
+import "./App.css";
+
+//참고로 현재여기 실행하면 오류뜬다....참고할것
 
 function App() {
+  const [data, setData] = useState([
+    { id: 1, name: "cos" },
+    { id: 2, name: "삽돌" },
+    { id: 3, name: "ㅋㅋ" },
+    { id: 4, name: "ㅁㅁ" },
+  ]);
+
+  const h1Element = Array.from({ length: data.length }).map(() => createRef);
+
+  const changeColor = () => {
+    h1Element[2].current.style.color = "red";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((data) => (
+        <h1 ref={h1Element[data.id - 1]}>{data.name}</h1>
+      ))}
+      <button onClick={changeColor}>클릭</button>
     </div>
   );
 }
